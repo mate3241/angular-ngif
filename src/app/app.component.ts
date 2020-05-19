9b52d219288381aeb2f02deb4554cb606f2be7b7
mate3241 <szabomatedenes@gmail.com>
2020-05-19 08:13:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngIf-ngClass-ngStyle';
+  length: number;
+  textInput = '';
+  editable = false;
+  color: string;
+  toggleEditable(){
+    this.editable = !this.editable;
+  }
+  isLongerThan9(input: string){
+    return input.length >= 10;
+  }
+  getColor(input: string){
+    return input.length >= 14 ? this.color : 'white';
+  }
 }
+/*Az egész eddigieket egy material kártyában jelenítsd meg és az inputoknak is adja material kinézetet
+	
+	
+	
+A kártyának legyen fejléce, amiben megadhatsz egy tetszőleges címet
+	
+	
+	
+A checkbox mellé tegyél egy fontawesome info ikont, aminek ha fölé viszed az egeret tooltipben megjelenik egy információ: “Csak akkor tölthető ki a mező ha be van pipálva” */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTooltipModule} from '@angular/material/tooltip'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    TextFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
